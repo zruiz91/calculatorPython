8261dfef02155f3a59ee2a1282bd0373da7932d1
Zach Ruiz <powersbooth10@gmail.com>
2023-07-12 13:47:24 -0500

    next step in lesson.

--- calculator.py
+++ calculator.py
@@ -54,6 +54,7 @@
 
 #Lesson follow along
 
+
 def add(n1, n2):
   return n1 + n2
 
@@ -66,6 +67,7 @@ def multiply(n1, n2):
 def divide(n1,n2):
   return n1 / n2
 
+  
 operations = {
   "+": add,
   "-": subtract,
@@ -73,8 +75,18 @@ operations = {
   "/": divide,
 }
 
-num1 = input("Whats the first number?: ")
-num2 = input("Whats the second number?: ")
 
-for operator in operations:
-  print(operator)
+num1 = int(input("Whats the first number?: "))
+
+for symbol in operations:
+  print(symbol)
+
+operation_symbol = input("Pick an operation from above: ")
+
+num2 = int(input("Whats the second number?: "))
+
+calculation_function = operations[operation_symbol]
+
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
