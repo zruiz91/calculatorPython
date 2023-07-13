f085439d528fb954d324914300973025e6b964d7
Zach Ruiz <powersbooth10@gmail.com>
2023-07-13 14:46:38 -0500

    refactored code to accept float numbers.

--- calculator.py
+++ calculator.py
@@ -55,7 +55,7 @@
 #Lesson follow along
 
 
-
+from art import logo
 
 def add(n1, n2):
   return n1 + n2
@@ -80,7 +80,9 @@ operations = {
 
 def calculator():
 
-  num1 = int(input("Whats the first number?: "))
+  print(logo)
+  
+  num1 = float(input("Whats the first number?: "))
 
   for symbol in operations:
     print(symbol)
@@ -92,9 +94,9 @@ def calculator():
   while calculate_again == True:
   
     operation_symbol = input("pick an operation: ")
-    num2 = int(input("Whats the next number?: "))
+    num2 = float(input("Whats the next number?: "))
     calculation_function = operations[operation_symbol]
-    answer = calculation_function(num1, num2)
+    answer = round(calculation_function(num1, num2), 2)
     print(f"{num1} {operation_symbol} {num2} = {answer}")
 
     decision = input(f"Type 'y' to continue calculating with {answer}, type 'n' to start a   new calculation.: ")
